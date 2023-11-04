cf3da06d165c8884ba029a72f071475d9f3c0442
Madeline Anson <madelineganson@gmail.com>
2023-11-04 18:21:33 -0400

    Adding color changer and reset button to website

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,26 +1,40 @@
-import {React, useState} from "react"
+import React, { useState } from "react";
 
-function Counter() {
-    const [count, setCount] = useState(10);
 
-    function increment() {
-        setCount(count+1);
+function Counter({ onUpdateSomeValue }) {
+  const [count, setCount] = useState(10);
+  const [color, setColor] = useState('pink');
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if (count !== 0) {
+      setCount(count - 1);
     }
-    
-    function decrement() {
-        if (count !== 0) {
-            setCount(count-1);
-        }
-    }
+  }
 
-    return(
-        <div id='counter'>
-            <button onClick={decrement}>-</button>
-            <h1>{count}</h1>
-            <button onClick={increment}>+</button>
-        </div>
-    );
-    
+  function reset(){
+    setCount(10);
+  }
+
+  function changeColor(){
+    setColor('#'+Math.random().toString(16).substr(-6));
+  }
+
+  return (
+    <div style={{background: color}} id='counter'>
+
+      <button onClick={decrement}>-</button>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <br></br>
+      <button onClick={reset}>Reset</button>
+      <br></br>
+      <button onClick={changeColor}>Change Color! :)</button>
+    </div>
+  );
 }
 
 export default Counter;
